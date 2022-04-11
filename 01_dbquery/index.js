2134b6f66b3dd3dd95eac822c8e289e0dc718242
Lukas_Skywalker <lukas.diener@hotmail.com>
2022-04-11 19:11:34 +0200

    Re-add steps

--- 01_dbquery/index.js
+++ 01_dbquery/index.js
@@ -1,5 +1,5 @@
 query("SELECT created_at, humidity, temperature FROM measurements WHERE sensor_id = 1", data => {
-  console.log(data);
-  var temperaturen = extract(data, 'temperature');
-  console.log(temperaturen);
+  console.log(data);                                /* start=2 */
+  var temperaturen = extract(data, 'temperature');  /* start=3 */
+  console.log(temperaturen);                        /* start=3 */
 });
