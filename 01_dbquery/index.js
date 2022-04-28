842b28376d84540fd9f0116329fbb8f5580c8f02
Lukas_Skywalker <lukas.diener@hotmail.com>
2022-04-28 18:51:22 +0200

    Add note to array

--- 01_dbquery/index.js
+++ 01_dbquery/index.js
@@ -1,5 +1,8 @@
 query("SELECT created_at, humidity, temperature FROM measurements WHERE sensor_id = 1", data => {
   console.log(data);                                /* start=2 */
-  var temperaturen = extract(data, 'temperature');  /* start=3 */
-  console.log(temperaturen);                        /* start=3 */
+  console.log(data[0]);                             /* start=3 */
+  console.log(data[1]);                             /* start=3 */
+  console.log(data[2]);                             /* start=3 */
+  var temperaturen = extract(data, 'temperature');  /* start=4 */
+  console.log(temperaturen);                        /* start=4 */
 });
